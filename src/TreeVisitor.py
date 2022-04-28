f685140b3b754fc5785c6a7e1ebff3b5a388bc37
cuberhaus <polcg@hotmail.es>
2022-04-28 17:06:00 +0200

    This is a placeholder

--- src/TreeVisitor.py
+++ src/TreeVisitor.py
@@ -8,41 +8,22 @@ class TreeVisitor(ExprVisitor):
     def __init__(self):
         self.nivell = 0
 
-    
-    
     def visitExpr(self, ctx):
         l = list(ctx.getChildren())
-
         if len(l) == 1:
             print("  " * self.nivell +
                   ExprParser.symbolicNames[l[0].getSymbol().type] +
                   '(' +l[0].getText() + ')')
-        elif l[1].getText() == '^':
-            print('  ' *  self.nivell + 'POT(^)')
-            self.nivell += 1
-            # for i in l:
-            #     print(i.getText())
-            self.visit(l[0])
-            self.visit(l[2])
-            self.nivell -= 1
-        elif l[1].getText() == '*':
-            print('  ' *  self.nivell + 'MULT(*)')
-            self.nivell += 1
-            # for i in l:
-            #     print(i.getText())
-            self.visit(l[0])
-            self.visit(l[2])
-            self.nivell -= 1
-        elif l[1].getText() == '+':
-            print('  ' *  self.nivell + 'MES(+)')
-            self.nivell += 1
-            # for i in l:
-            #     print(i.getText())
-            self.visit(l[0])
-            self.visit(l[2])
-            self.nivell -= 1
-        elif l[1].getText() == '-':
-            print('  ' *  self.nivell + 'MINUS(-)')
+        elif len(l) > 1:
+            if l[1].getText() == '^':
+                print('  ' *  self.nivell + 'POT(^)')
+            elif l[1].getText() == '*':
+                print('  ' *  self.nivell + 'MULT(*)')
+            elif l[1].getText() == '+':
+                print('  ' *  self.nivell + 'MES(+)')
+            elif l[1].getText() == '-':
+                print('  ' *  self.nivell + 'MINUS(-)')
+
             self.nivell += 1
             # for i in l:
             #     print(i.getText())
