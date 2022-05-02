3d8f2935cf5570572e14d7298f64e9af251fa1ab
cuberhaus <polcg@hotmail.es>
2022-05-02 18:10:19 +0200

    This is a placeholder

--- src/TreeVisitor.py
+++ src/TreeVisitor.py
@@ -40,16 +40,6 @@ class TreeVisitor(ExprVisitor):
             self.visit(l[2])
             self.nivell -= 1
 
-    # Visit a parse tree produced by ExprParser#root.
-    def visitRoot(self, ctx:ExprParser.RootContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#expression.
-    def visitExpression(self, ctx:ExprParser.ExpressionContext):
-        return self.visitChildren(ctx)
-
-
     # Visit a parse tree produced by ExprParser#assign_id.
     def visitAssign_id(self, ctx:ExprParser.Assign_idContext):
         l = list(ctx.getChildren())
@@ -57,45 +47,5 @@ class TreeVisitor(ExprVisitor):
         # value = self.visit(l[2])
         for i in l:
             print(l)
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#newline.
-    def visitNewline(self, ctx:ExprParser.NewlineContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#minus.
-    def visitMinus(self, ctx:ExprParser.MinusContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#mult.
-    def visitMult(self, ctx:ExprParser.MultContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#pot.
-    def visitPot(self, ctx:ExprParser.PotContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#num.
-    def visitNum(self, ctx:ExprParser.NumContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#mes.
-    def visitMes(self, ctx:ExprParser.MesContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#id.
-    def visitId(self, ctx:ExprParser.IdContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by ExprParser#assign.
-    def visitAssign(self, ctx:ExprParser.AssignContext):
         return self.visitChildren(ctx)
 
